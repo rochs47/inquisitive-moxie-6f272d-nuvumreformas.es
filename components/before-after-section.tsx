bd0448f4d4d3153282622ad2a4840caefa7370f9
rochs47 <71324404+rochs47@users.noreply.github.com>
2026-07-15 11:50:59 +0200

    cocina1
    cocina1

--- components/before-after-section.tsx
+++ components/before-after-section.tsx
@@ -2,8 +2,8 @@ export function BeforeAfterSection() {
   // Aquí es donde añades tus fotos. 
   // Asegúrate de que la ruta coincida exactamente con la carpeta que creaste en 'public'
   const images = [
-    { src: "/images/piso-badalona-julio-2026/cocina-1.jpeg", alt: "Reforma " },
-    { src: "/images/piso-badalona-julio-2026/cocina-2.jpeg", alt: "Reforma cocina" },
+    { src: "/images/piso-badalona-julio-2026/cocina1.jpeg", alt: "Reforma " },
+    { src: "/images/piso-badalona-julio-2026/cocina2.jpeg", alt: "Reforma cocina" },
     // Añade aquí todas las que quieras
   ];
 
