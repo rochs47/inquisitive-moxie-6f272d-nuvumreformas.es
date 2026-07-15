d9584f409211ef0e03f7a8317009cf4bf0245187
rochs47 <71324404+rochs47@users.noreply.github.com>
2026-07-15 13:27:38 +0200

    Update FinishedProjectsSection.tsx

--- components/FinishedProjectsSection.tsx
+++ components/FinishedProjectsSection.tsx
@@ -1,6 +1,6 @@
 export function FinishedProjectsSection() {
   const images = [
-    { src: "public/images/cocina-1.jpg", alt: "Cocina terminada" },
+    { src: "public/images/cocina1.jpeg", alt: "Cocina terminada" },
   ];
 
   return (
